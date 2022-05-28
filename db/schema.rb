1236e40276bf3d4dc89636ac037681a5dd9da208
Chinmay Lohani <chinmaylohani27@gmail.com>
2022-05-29 01:38:39 +0530

    tasks creation successful

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_28_195518) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_28_200730) do
+  create_table "tasks", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.string "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
